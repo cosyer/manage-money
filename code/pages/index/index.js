8143fd05f9b8f78ce78120aab67036e87a4ab46d
陈宇 <535509852@qq.com>
2018-06-28 01:55:26 +0800

    处理getUserInfo

--- code/pages/index/index.js
+++ code/pages/index/index.js
@@ -1,5 +1,5 @@
 //查询详情
-var searchBalance=function(that){
+var searchBalance = function(that) {
   var userId = that.data.userInfo.nickName;
   wx.request({
     url: "http://127.0.0.1:3000/balance",
@@ -8,66 +8,86 @@ var searchBalance=function(that){
       "Content-Type": "application/x-www-form-urlencoded"
     },
     data: {
-      "wechatname": userId
+      wechatname: userId
     },
 
-    success: function (res) {
+    success: function(res) {
       console.log(res.data);
-      if (res.statusCode == '200') {
+      if (res.statusCode == "200") {
         that.setData({
           balance: res.data.balance
-        })
-      }
-      else {
+        });
+      } else {
         wx.showToast({
-          title: '查询失败',
+          title: "查询失败",
           image: "../../images/icon-no.png",
           mask: true,
           duration: 1000
-        })
+        });
       }
     }
-  })
-}
+  });
+};
 
 Page({
   data: {
-    userInfo:[],
-    balance:0
+    userInfo: [],
+    balance: 0,
+    canIUse: wx.canIUse("button.open-type.getUserInfo"),
+    showShouquan: true
   },
 
   //添加一笔新账单
-  bindNewTap: function () {
+  bindNewTap: function() {
     wx.navigateTo({
-      url: '../new/new'
-    })
+      url: "../new/new"
+    });
   },
 
   //理财产品推荐
-  bindNewTapTwo: function () {
+  bindNewTapTwo: function() {
     wx.navigateTo({
-      url: '../finance/finance'
-    })
+      url: "../finance/finance"
+    });
   },
 
-  onLoad: function () {
-    var that = this
+  onLoad: function() {
+    // 查看是否授权
+    var that = this;
+    wx.getSetting({
+      success: function(res) {
+        console.log(res.authSetting["scope.userInfo"]);
+        if (res.authSetting["scope.userInfo"]) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          searchBalance(that);
+          wx.getUserInfo({
+            success: function(res) {
+              console.log(res);
+              that.setData({
+                userInfo: res.userInfo
+              });
+              // getApp().globalData.userInfo = res.userInfo; //将授权信息传递给全局变量
+            }
+          });
+        }
+      }
+    });
 
     //调用应用实例的方法获取全局数据
-    var app = getApp()
-    app.getUserInfo(function (userInfo) {
-      //更新数据
-      that.setData({
-        userInfo: userInfo
-      })
-      searchBalance(that);
-    })
-    
-    
+    // var app = getApp()
+    // app.getUserInfo(function (userInfo) {
+    //   //更新数据
+    //   that.setData({
+    //     userInfo: userInfo
+    //   })
+    // })
+  },
+  bindGetUserInfo: function(e) {
+    this.data.userInfo = e.detail.userInfo; //将授权信息传递给全局变量
   },
 
-  onHide:function(){
-    var that=this;
+  onHide: function() {
+    var that = this;
     searchBalance(that);
-  },
-})
+  }
+});
